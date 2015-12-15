6145abe9c3a12cd547b88673b374eca1c91db2d5
Egor Artemov <egor.artemov@gmail.com>
2015-12-15 18:15:07 +0300

    Convert Unicode argv to UTF8 on Windows

--- src/entry_main.c
+++ src/entry_main.c
@@ -1,6 +1,4 @@
 #include <KD/kd.h>
-#include <stdlib.h>
-#include <stdio.h>
 
 int main (int argc, char *argv[])
 {
--- src/entry_winmain.c
+++ src/entry_winmain.c
@@ -15,12 +15,40 @@
 #endif /* _MSC_VER */
 #define UNUSED(x) (void)(x)
 
+static char *get_utf8 (LPCWSTR string)
+{
+    char *UTF8String = NULL;
+    int UTF8StringLength = WideCharToMultiByte (CP_UTF8, WC_ERR_INVALID_CHARS,
+                           string, -1, UTF8String, 0, NULL, NULL);
+    UTF8String = (char *) kdMalloc (sizeof (char) * UTF8StringLength);
+    WideCharToMultiByte (CP_UTF8, WC_ERR_INVALID_CHARS, string, -1, UTF8String,
+                         UTF8StringLength, NULL, NULL);
+    return UTF8String;
+}
+
+#if defined(KD_WINDOW_SUPPORTED)
 int CALLBACK WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance,
                       LPSTR lpCmdLine, int nCmdShow)
 {
+    int argc = __argc;
+    wchar_t **wargv = __wargv;
     UNUSED (hInstance);
     UNUSED (hPrevInstance);
     UNUSED (lpCmdLine);
     UNUSED (nCmdShow);
-    return kdMain (__argc, (const KDchar * const *)__argv);
+#else
+int wmain (int argc, wchar_t *wargv[ ], wchar_t *envp[])
+{
+#endif
+    int result = 0;
+    char **argvu8 = (char **) kdMalloc (sizeof (char *) * argc);
+    for (int i = 0; i < argc; i++) {
+        argvu8[i] = get_utf8 (wargv[i]);
+    }
+    result = kdMain (argc, (const KDchar * const *)argvu8);
+    for (int i = 0; i < argc; i++) {
+        kdFree (argvu8[i]);
+    }
+    kdFree (argvu8);
+    return result;
 }
