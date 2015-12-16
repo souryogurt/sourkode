66b92a3f5724e2424bde71c25df9b008566461b0
Egor Artemov <egor.artemov@gmail.com>
2015-12-17 00:37:44 +0300

    Fix win32 build

--- src/entry_winmain.c
+++ src/entry_winmain.c
@@ -1,4 +1,3 @@
-#include <KD/kd.h>
 #ifdef _MSC_VER
 #pragma warning( push )
 #pragma warning( disable: 4668 )
@@ -6,50 +5,82 @@
 #pragma warning( disable: 4255 )
 #pragma warning( disable: 4820 )
 #endif /* _MSC_VER */
-#include <tchar.h>
+#include <KD/kd.h>
 #include <windows.h>
-#include <stdlib.h>
 #ifdef _MSC_VER
 #pragma warning( pop )
 #pragma warning( disable: 4710 )
 #endif /* _MSC_VER */
 #define UNUSED(x) (void)(x)
 
-static char *get_utf8 (LPCWSTR string)
+/** Convert UTF16 string to UTF8 string
+ * @param utf16_string UTF16 string terminated by 0
+ * @returns new UTF8 string, NULL otherwise
+ */
+static char *get_utf8 (LPCWSTR utf16_string)
 {
-    char *UTF8String = NULL;
-    int UTF8StringLength = WideCharToMultiByte (CP_UTF8, WC_ERR_INVALID_CHARS,
-                           string, -1, UTF8String, 0, NULL, NULL);
-    UTF8String = (char *) kdMalloc (sizeof (char) * UTF8StringLength);
-    WideCharToMultiByte (CP_UTF8, WC_ERR_INVALID_CHARS, string, -1, UTF8String,
-                         UTF8StringLength, NULL, NULL);
-    return UTF8String;
+    char *result = NULL;
+    if (utf16_string) {
+        int result_length = WideCharToMultiByte (CP_UTF8, WC_ERR_INVALID_CHARS,
+                            utf16_string, -1, result, 0, NULL, NULL);
+        result = (char *) kdMalloc (sizeof (char) * result_length);
+        if (result) {
+            WideCharToMultiByte (CP_UTF8, WC_ERR_INVALID_CHARS, utf16_string, -1, result,
+                                 result_length, NULL, NULL);
+        }
+    }
+    return result;
 }
 
-#if defined(KD_WINDOW_SUPPORTED)
-int CALLBACK WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance,
-                      LPSTR lpCmdLine, int nCmdShow)
+/** Convert array of UTF16 strings to array of UTF8 strings
+ * @param strings Array of UTF16 strings
+ * @param count Number of elements of array
+ * @returns new array of UTF8 strings, NULL otherwise
+ */
+static char **get_utf8_array (wchar_t **strings, int count)
 {
-    int argc = __argc;
-    wchar_t **wargv = __wargv;
-    UNUSED (hInstance);
-    UNUSED (hPrevInstance);
-    UNUSED (lpCmdLine);
-    UNUSED (nCmdShow);
-#else
+    char **result = NULL;
+    if (strings && (count > 0)) {
+        result = (char **) kdMalloc (sizeof (char *) * count);
+        if (result) {
+            int i = 0;
+            while (i < count) {
+                result[i] = get_utf8 (strings[i]);
+                if (result[i] == NULL) {
+                    kdFree (result);
+                    return NULL;
+                }
+                i++;
+            }
+        }
+    }
+    return result;
+}
+
+/** Frees array of strings
+ * @param strings Array of strings
+ * @param count Number of elements of array
+ */
+static void free_utf8_array (char **strings, int count)
+{
+    if (strings) {
+        int i = 0;
+        while (i < count) {
+            kdFree (strings[i]);
+            i++;
+        }
+        kdFree (strings);
+    }
+}
+
 int wmain (int argc, wchar_t *wargv[ ], wchar_t *envp[])
 {
-#endif
-    int result = 0;
-    int i = 0;
-    char **argvu8 = (char **) kdMalloc (sizeof (char *) * argc);
-    for (i = 0; i < argc; i++) {
-        argvu8[i] = get_utf8 (wargv[i]);
+    int result = 1;
+    char **argv = get_utf8_array (wargv, argc);
+    UNUSED (envp);
+    if (argv) {
+        result = kdMain (argc, (const KDchar * const *)argv);
+        free_utf8_array (argv, argc);
     }
-    result = kdMain (argc, (const KDchar * const *)argvu8);
-    for (i = 0; i < argc; i++) {
-        kdFree (argvu8[i]);
-    }
-    kdFree (argvu8);
     return result;
 }
